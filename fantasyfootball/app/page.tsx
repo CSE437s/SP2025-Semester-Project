563a5efb4923e84b455469143c00d7d68d9e6cf6
RandyDi <randydiaz9050@gmail.com>
2024-09-17 18:49:33 -0500

    Quick Update

--- fantasyfootball/app/page.tsx
+++ fantasyfootball/app/page.tsx
@@ -3,10 +3,15 @@ import { NextPage } from "next";
 import styles from '../styles/homePageLayout.module.css';
 import { useState } from "react";
 import { Button } from "@mui/material";
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import VolumeMuteIcon from '@mui/icons-material/VolumeMute';
+
 
 const HomePage: NextPage = () => {
 
   const [isMute, setIsMute] = useState<boolean>(false);
+
+  
 
   const muteSound = () => {
     setIsMute(false);
@@ -16,7 +21,11 @@ const HomePage: NextPage = () => {
     <main>
     <div className={styles.App}>
       <h1 className={styles.title}>Hello World</h1>
-      <Button></Button>
+      {isMute ? (
+        <Button onClick={muteSound}><VolumeMuteIcon/></Button>
+      ): (
+        <Button onClick={muteSound}><VolumeUpIcon/></Button>
+      )}
     </div>
     </main>
     </>
